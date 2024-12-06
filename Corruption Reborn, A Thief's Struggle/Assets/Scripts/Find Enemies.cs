9c08d521debe519fd4bb6d8c15a0899be0430027
Angel Espinoza <62255918+student-ai@users.noreply.github.com>
2024-12-06 09:21:06 -0800

    am tired, testing stuff

--- Corruption Reborn, A Thief's Struggle/Assets/Scripts/Find Enemies.cs
+++ Corruption Reborn, A Thief's Struggle/Assets/Scripts/Find Enemies.cs
@@ -5,23 +5,28 @@ using TMPro;
 
 public class FindEnemies : MonoBehaviour 
 {
+    [SerializeField]
+    GameObject player;
     private void Start()
     {
+        string playername = player.name;
+        Debug.Log(playername);
     }
 
     void Update()
     {
         // find enemies on the screen
-        GameObject[] positions = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
 
         // for every enemy, find their position and save it (maybe in a list?)
         // and for now, debug it to show the code works
-        for(int i = 0; i < positions.Length; i++)
+        for(int i = 0; i < enemies.Length; i++)
         {
 
             Vector2 tempEnemy;
-            tempEnemy = positions[i].transform.position;
-            Debug.Log(tempEnemy);
+            tempEnemy = enemies[i].transform.position;
+            // Debug.Log(tempEnemy);
+            
         }
     }
 
