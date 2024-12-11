f72e9286a71e4e4548d08e89d58396ab5563f9b6
Angel Espinoza <62255918+student-ai@users.noreply.github.com>
2024-12-11 08:14:20 -0800

    updated the main menu script

--- Corruption Reborn, A Thief's Struggle/Assets/Scripts/UI/MainMenu.cs
+++ Corruption Reborn, A Thief's Struggle/Assets/Scripts/UI/MainMenu.cs
@@ -1,10 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +14,47 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
 
-    }
-    public void Play()
+    // start; begin game (at level one)
+    public void StartOne()
     {
-        SceneManager.LoadScene("samplescene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("LevelOne");
     }
+
+    // start; begin game (at level two)
+    public void StartTwo()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("LevelTwo");
+    }
+
+    public void StartThree()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("LevelThree");
+    }
+
+    // quit game
     public void QuitGame()
     {
         Application.Quit();
     }
-}
 
+    // send player to level select screen,
+    // so they can choose between the levels (even if there's only 2)
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("MMLevelSelect");
+    }
+
+    // allows the player to return to the main screen
+    public void BackMM()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
